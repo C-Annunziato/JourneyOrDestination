5e35db7d60e1c67f12b2bc6153732433200e45a2
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2022-10-01 01:04:17 -0400

    nav working

--- app/src/main/java/com/example/journeyordestination/view/MainActivity.kt
+++ app/src/main/java/com/example/journeyordestination/view/MainActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.journeyordestination.R
 import com.example.journeyordestination.databinding.ActivityMainBinding
@@ -25,33 +25,36 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val toolbar:androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+
         setSupportActionBar(toolbar)
-//
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-//        if (navHostFragment != null) {
-//            navController = navHostFragment.findNavController()
-//        }
-//
-//        setupActionBarWithNavController(navController)
-//    }
-//
+        setupActionBarWithNavController(navController)
     }
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.app_bar_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.menu_add_contact -> Toast.makeText(this, "hey", Toast.LENGTH_SHORT).show()
-//            navController.navigate(R.id.addDestinationFragment)
-        }
-        return true
+
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
-//
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
 
 }
+
+
+
+
+
